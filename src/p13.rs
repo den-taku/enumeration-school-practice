098a43c44f836e0d987bea504abf7dc404be2f72
den-taku <dentaku1412@gmail.com>
2022-05-17 01:16:03 +0900

    Fix forward traverse

--- src/p13.rs
+++ src/p13.rs
@@ -27,8 +27,9 @@ pub fn comp(_g: Graph) {
 // 対象Sの親：V\SのうちSと合わせてcliqueになるような頂点のうち最小のものをSに加えた集合
 fn a(_s: Vec<usize>, _g: Graph) {
     // 1. Sの中でcut-vertexでないものを列挙：lowlink, O(V + E)
-    // 2. v := min{min{v \in Sで V\S の一つのみと隣接する}, max{v \in Sで vは隣接しないがvの次が2個以上と隣接する}}：O(V + E)
-    // 3. reverse traverse：s \in S がcut-vertexでない O(1)
-    // 4. forward traverse：s \in S が 2で定めたv以下 O(1)
+    // 2. v_1 := min{v \in V\S; Sと隣接する}：O(V + E)
+    // 3. v_1が１頂点s_1とのみ隣接 → v_2 := min{v \in V\S; S\{s_1}と隣接する}：O(V + E)
+    // 4. reverse traverse：s \in S がcut-vertexでない O(1)
+    // 5. forward traverse：v_1が２頂点以上と隣接 → v_1以下，v_1が１頂点のみと隣接 → v_1以下またはs_1でs_1 <= v_2 O(1)
     unimplemented!()
 }
