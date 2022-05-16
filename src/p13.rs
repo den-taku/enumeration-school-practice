ef1b42cbc2eba6245e89ff7b62f51f1edb3ac384
den-taku <dentaku1412@gmail.com>
2022-05-17 00:51:37 +0900

    Add time complexity

--- src/p13.rs
+++ src/p13.rs
@@ -18,7 +18,7 @@ pub struct Edges {
 // 最悪時線形時間遅延アルゴリズム
 pub fn comp(_g: Graph) {
     println!("Practice 13");
-    // Gを連結成分C_1, C_2, ..., C_kに分解する
+    // Gを連結成分C_1, C_2, ..., C_kに分解する O(V + E)
     // 連結成分C_i毎にa(C_i, G)を呼び出して停止
     unimplemented!()
 }
@@ -26,9 +26,9 @@ pub fn comp(_g: Graph) {
 // 根：連結成分
 // 対象Sの親：V\SのうちSと合わせてcliqueになるような頂点のうち最小のものをSに加えた集合
 fn a(_s: Vec<usize>, _g: Graph) {
-    // 1. Sの中でcut-vertexでないものを列挙：lowlink，O(V + E)
+    // 1. Sの中でcut-vertexでないものを列挙：lowlink, O(V + E)
     // 2. v := min{min{v \in Sで V\S の一つのみと隣接する}, max{v \in Sで vは隣接しないがvの次が2個以上と隣接する}}：O(V + E)
     // 3. reverse traverse：s \in S がcut-vertexでない O(1)
-    // 4. forward traverse：s \in S が 2で定めたv以下
+    // 4. forward traverse：s \in S が 2で定めたv以下 O(1)
     unimplemented!()
 }
