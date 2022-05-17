e1df8649af1348a24d9956bc65527a0a8f55a303
den-taku <dentaku1412@gmail.com>
2022-05-17 15:07:26 +0900

    Fix p13

--- src/p13.rs
+++ src/p13.rs
@@ -1,25 +1,23 @@
 #![allow(dead_code)]
-
 /// 入力：無向グラフ G
-/// 出力：Gのクリーク全て
+/// 出力：Gのクリーク(完全部分グラフ)全て
 ///
 /// 最悪時線形時間遅延アルゴリズム
 pub fn comp(_g: Graph) {
     println!("Practice 13");
-    // Gを連結成分C_1, C_2, ..., C_kに分解する O(V + E)
-    // 連結成分C_i毎にa(C_i, G)を呼び出して停止
+    // a(∅)を呼び出して停止
     unimplemented!()
 }
 
-// 根：連結成分
-// 対象Sの親：V\SのうちSと合わせてcliqueになるような頂点のうち最小のものをSに加えた集合
+// 根：∅
+// 対象Sの親：Sのうち一番添字の小さいものを取り除いたもの（cliqueの単調性より）
 fn a(_s: Vec<usize>, _g: Graph) {
     // 0. Sを出力
-    // 1. Sの中でcut-vertexでないものを列挙：lowlink, O(V + E)
-    // 2. v_1 := min{v \in V\S; Sと隣接する}：O(V + E)
-    // 3. v_1が１頂点s_1とのみ隣接 → v_2 := min{v \in V\S; S\{s_1}と隣接する}：O(V + E)
-    // 4. reverse traverse：s \in S がcut-vertexでない O(1)
-    // 5. forward traverse：v_1が２頂点以上と隣接 → v_1以下，v_1が１頂点のみと隣接 → v_1以下またはs_1でs_1 <= v_2 O(1)
+    // 1. V\Sに対し配列を用意する．全て0で初期化．
+    // 2. 辺集合Eに対し以下を行う
+    //  2-1. 辺の片方がS，もう片方がV\Sに接続する時，V\Sの配列の値をインクリメントする
+    // 3. reverse traverse：V\Sのうちmin Sより小さいもの : 各遷移はO(1)
+    // 4. forward traverse：配列の値が|S|（その頂点はSの全ての頂点に隣接することを意味する）: O(1)
     unimplemented!()
 }
 
